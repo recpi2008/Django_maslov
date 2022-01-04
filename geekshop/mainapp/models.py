59ddeb9c2d5bfd50a4aa47537c0e7f674777172d
eles <ivaneleskin2019@gmail.com>
2022-01-04 14:52:42 +0200

    add

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=64, unique=True, verbose_name='название')
+    description = models.TextField(blank=True, null=True, verbose_name='описание')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ('-id',)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    image = models.ImageField(upload_to='products', blank=True, null=True, verbose_name='картинка')
+    short_desc = models.CharField(max_length=256, verbose_name='краткое описание')
+    description = models.TextField(blank=True, null=True, verbose_name='описание')
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0, verbose_name='цена')
+    quantity = models.PositiveIntegerField(default=0, verbose_name='количество')
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, verbose_name='категория')
+
+    def __str__(self):
+        return f'{self.name} | {self.category.name}'
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
+from mainapp.models import Product, ProductCategory
+
 
 def index(request):
     context = {
         'title':'Главная',
+        'products': Product.objects.all(),
     }
     return render(request, 'mainapp/index.html',context)
 
@@ -14,60 +17,10 @@ def contact(request):
     }
     return render(request, 'mainapp/contact.html',context)
 
-
-
-links_menu = [
-    {
-        'url': 'products',
-        'title': 'Все',
-    },
-    {
-        'url': 'products_home',
-        'title': 'дом',
-    },
-    {
-        'url': 'products_office',
-        'title': 'офис',
-    },
-    {
-        'url': 'products_modern',
-        'title': 'модерн',
-    },
-    {
-        'url': 'products_classic',
-        'title': 'классика',
-    },
-]
-
-
-def products(request):
+def products(request, pk=None):
     context = {
         'title':'Продукты',
-        'links_menu': links_menu,
+        'links_menu': ProductCategory.objects.all()
     }
     return render(request, 'mainapp/products.html', context=context)
 
-def products_home(request):
-    context = {
-        'links_menu': links_menu,
-    }
-    return render(request, 'mainapp/products.html', context=context)
-
-def products_office(request):
-    context = {
-        'links_menu': links_menu,
-    }
-    return render(request, 'mainapp/products.html', context=context)
-
-def products_modern(request):
-    context = {
-        'links_menu': links_menu,
-    }
-    return render(request, 'mainapp/products.html', context=context)
-
-
-def products_classic(request):
-    context = {
-        'links_menu': links_menu,
-    }
-    return render(request, 'mainapp/products.html', context=context)
